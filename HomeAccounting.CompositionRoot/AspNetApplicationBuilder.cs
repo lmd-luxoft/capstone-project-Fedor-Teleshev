f2111477d70777a644484aad47ecf13d5941876b
Fedor Teleshev <fediat@yandex.ru>
2021-06-08 13:56:25 +0300

    Infrastructure

---
+++ HomeAccounting.CompositionRoot/AspNetApplicationBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAccounting.CompositionRoot
+{
+    public class AspNetApplicationBuilder : AbstractApplicationBuilder
+    {
+        public AspNetApplicationBuilder(IServiceCollection services) : base(services) { }
+        
+        protected override void RegisterBusinessLogic()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterDataSource()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterInfrastructure()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
